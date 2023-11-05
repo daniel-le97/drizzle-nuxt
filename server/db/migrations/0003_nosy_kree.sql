6a32cd1964f8e07b2b3ac082a8bc400db96ff627
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-11-05 11:50:00 -0700

    make todos work

---
+++ server/db/migrations/0003_nosy_kree.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "todos" ADD PRIMARY KEY ("id");--> statement-breakpoint
+ALTER TABLE "todos" ALTER COLUMN "id" SET NOT NULL;
--- server/utils/dbFields.ts
+++ server/utils/dbFields.ts
@@ -1,3 +1,4 @@
+import { sql } from 'drizzle-orm';
 import * as pg from 'drizzle-orm/pg-core'
 
 /**
@@ -7,7 +8,8 @@ import * as pg from 'drizzle-orm/pg-core'
 export const createdAt = pg.timestamp('createdAt', { withTimezone: true, mode: 'date' }).defaultNow()
 export const updatedAt = pg.timestamp('updatedAt', { withTimezone: true, mode: 'date' }).defaultNow()
 export const datetime = (name: string) => pg.timestamp(name, { withTimezone: true, mode: 'date' })
-export const id = pg.char('id').primaryKey()
+// this should be serial but it is broken, this works tho for getting random ids, as auto increment is not working
+export const id = pg.varchar('id').primaryKey().$default(() => sql`gen_random_uuid()`)
 
 // Timestamps is inspired by the Mongoose "timestamps" option to provide automated timestamps for create and update actions
 export const timestamps = {
