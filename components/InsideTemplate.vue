16182d35171dffc38518b7ea9c017943b37ae55c
tungle0319 <tung.le0319@gmail.com>
2023-11-06 22:21:28 -0700

    modification

--- components/InsideTemplate.vue
+++ components/InsideTemplate.vue
@@ -49,21 +49,17 @@ const sections = [
 
   },
   {
-    title: 'Extensive ways to customize to taste',
-    subtitle: 'Gain the ease and advantage by incorporating Nuxt Modules',
-    image: 'https://images.unsplash.com/photo-1665686377065-08ba896d16fd?auto=format&fit=crop&q=80&w=1170&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Pre built components',
+    subtitle: 'Premade components that I find myself using often that I enjoy',
+    image: 'https://images.unsplash.com/photo-1555664424-778a1e5e1b48?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     list: [
       {
-        header: 'High level of customization',
-        content: `Unlock a wealth of customization options to effortlessly tailor the template to your brand's identity and unique requirements, ensuring a distinct and efficient website.`,
+        header: 'Dynamic Routing',
+        content: `Prebuilt Testimonials and Pricing plan sections that are easily customizable, have some added animation for a slight pop if that's to your taste.`,
       },
       {
-        header: 'Daisy UI Component Library',
-        content: `Elevate your web design with the Daisu UI Component Library. Experience stunning, customizable components that effortlessly match your brand's identity.Say goodbye to complex HTML class names and enjoy a selection of beautifully crafted tailwind components.`,
-      },
-      {
-        header: 'Tired of just dark mode?',
-        content: '29 amazing color themes for a distinct, aesthetically pleasing website. Explore design possibilities and transform your online presence with Daisy UI. Check it out on the NavBar component.',
+        header: 'Authentication Pre Configured  for Github',
+        content: `Just Plugin your Keys into a .env and then test logging in with Github!`,
       },
       {
         header: 'Page Layout and Transitions',
@@ -85,7 +81,7 @@ const sections = [
       subtitle="What this templates offers with the Power of Nuxt, Drizzle and Tailwind." />
     <div class="flex flex-col items-center justify-center space-y-24">
       <div v-for="(section, index) in sections" :key="section.title" class="flex items-center justify-center space-x-4">
-        <div :class="index % 2 === 0 ? ' order-1' : ' order-0'" class=" w-1/2 flex items-center justify-center">
+        <div :class="index % 2 === 0 ? ' order-1' : ' order-0'" class="  lg:w-1/3 w-1/2 flex items-center justify-center">
           <NuxtImg :src="section.image" alt="accent image" />
         </div>
         <div :class="index % 2 === 1 ? ' order-1' : ' order-0'" class=" w-1/2 flex flex-col p-5">
